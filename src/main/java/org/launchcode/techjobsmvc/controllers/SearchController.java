a83195decdac1bd3ccc042d0e5fe648a059ac02b
Vennen-v <chgolliday@gmail.com>
2024-11-11 18:18:56 -0600

    All test pass

--- src/main/java/org/launchcode/techjobsmvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/techjobsmvc/controllers/SearchController.java
@@ -32,7 +32,7 @@ public class SearchController {
   @PostMapping("results")
   public String displaySearchResults(Model model, @RequestParam String searchType, @RequestParam String searchTerm){
     ArrayList<Job> jobs;
-    if(searchTerm.equals("")){
+    if(searchTerm.equals("all") || searchTerm.equals("")){
         jobs= JobData.findAll();
     }else{
         jobs=JobData.findByColumnAndValue(searchType, searchTerm);
